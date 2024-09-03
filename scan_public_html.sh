50c6f6493fe85320573db06d2700fba8022d471a
gwynbliedd <abdulla.amash@gmail.com>
2024-09-03 09:45:10 +0300

    email works, scan works

--- scan_public_html.sh
+++ scan_public_html.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 directory_to_search="public_html"
-time_threshold=0.18 
-recipient_email="" # Set Email
+time_threshold=0.25  # quarter min = 15 sec
+
+emails_path="emails.txt"
+mapfile -t recipient_email < <(grep -Ev "^$" "$emails_path")
 
 full_path=($(find /home -type d -name "$directory_to_search" 2> /dev/null)) 
 temp_file=$(mktemp)
 
 for path in "${full_path[@]}"; do
- find "$path" -type f -mmin -$time_threshold 2> /dev/null >> "$temp_file"  
+ find "$path" -depth -type f -mmin -$time_threshold 2> /dev/null >> "$temp_file"  
 done
 
 if [[ -s "$temp_file" ]]; then 
-  mail -s "Files Created Last 10sec" "$recipient_email" < "$temp_file" 
+  mail -s "Files Created Last 10sec" "${recipient_email[@]}" < "$temp_file" 
 fi
 
 rm "$temp_file"
