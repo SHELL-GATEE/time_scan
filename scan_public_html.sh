bad5343bbf894e48032a0f24e635b472d3cdbe2e
gwynbliedd <abdulla.amash@gmail.com>
2024-09-03 10:47:18 +0300

    add runtime eval

--- scan_public_html.sh
+++ scan_public_html.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+start_time=$(date +%s)
 directory_to_search="public_html"
 time_threshold=0.25  # quarter min = 15 sec
 
 emails_path="emails.txt"
 mapfile -t recipient_email < <(grep -Ev "^$" "$emails_path")
 
-full_path=($(find /home -type d -name "$directory_to_search" 2> /dev/null)) 
+
+full_path=($(find /home -maxdepth 2 -type d -name "$directory_to_search" 2> /dev/null)) 
 temp_file=$(mktemp)
 
 for path in "${full_path[@]}"; do
  find "$path" -depth -type f -cmin -$time_threshold 2> /dev/null >> "$temp_file"  
 done
 
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time)) 
 if [[ -s "$temp_file" ]]; then 
-  mail -s "Files Created Last 10sec" "${recipient_email[@]}" < "$temp_file" 
+  mail -s "Files Created Last 10sec, Runtime ($elapsed_time secondes)" "${recipient_email[@]}" < "$temp_file"
+
+else
+
+  echo No results!   
 fi
 
 rm "$temp_file"
