e19b87c9914ea96fbc699539484bb65a06abb881
gwynbliedd <abdulla.amash@gmail.com>
2024-09-03 12:17:43 +0300

    add exclude method to script

--- scan_public_html.sh
+++ scan_public_html.sh
@@ -8,19 +8,32 @@ mapfile -t recipient_email < <(grep -Ev "^$" "$emails_path")
 
 
 full_path=($(find /home -maxdepth 2 -type d -name "$directory_to_search" 2> /dev/null)) 
+
 temp_file=$(mktemp)
 
+##########################EXCLUDE##############################
+exclusions_path="exclude.txt"
+mapfile -t exclusions < <(grep -Ev "^$" "$exclusions_path")
+
+
+exclusion_patterns=""
+for exclusion in "${exclusions[@]}"; do
+    exclusion_patterns+=" -not -path $exclusion "
+done
+###############################################################
+
 for path in "${full_path[@]}"; do
- find "$path" -depth -type f -cmin -$time_threshold 2> /dev/null >> "$temp_file"  
+  find "$path"  -depth -type f -mmin -$time_threshold $exclusion_patterns   >> "$temp_file"
 done
 
+
+cat "$temp_file"
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time)) 
 if [[ -s "$temp_file" ]]; then 
   mail -s "Files Created Last 10sec, Runtime ($elapsed_time secondes)" "${recipient_email[@]}" < "$temp_file"
 
 else
-
   echo No results!   
 fi
 
