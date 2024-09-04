c3ae71c4624d3d4e259df32147254a124a169ab7
gwynbliedd <abdulla.amash@gmail.com>
2024-09-04 10:50:55 +0300

    add cron auto, loop emails

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # time_scan
+
+
+# to run as a cronjob every 10 sec
+```bash
+* * * * * for i in {1..6}; do /time_scan/scan_public_html.sh & sleep 10; done
+```
--- scan_public_html.sh
+++ scan_public_html.sh
@@ -1,51 +1,54 @@
 #!/bin/bash
-script_name="scan_10"
+### VARIABLES ###
+script_name="scan_public_html.sh"
 start_time=$(date +%s)
 directory_to_search="public_html"
-time_threshold=0.32  # = 20 sec
-
-emails_path="emails.txt"
-mapfile -t recipient_email < <(grep -Ev "^$" "$emails_path")
-
+time_threshold=0.25  # = 15 sec
 server_ip=$(curl -s ifconfig.me | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}')
 hostname=$(hostname)
-
-
-full_path=$(find /home -maxdepth 2 -type d -name "$directory_to_search" 2> /dev/null) 
+emails_path="emails.txt"
+mapfile -t recipient_email < <(grep -Ev "^$" "$emails_path")
+full_path=$(find /home/*/public_html -type d -name "$directory_to_search" 2> /dev/null) 
 temp_file=$(mktemp)
+###-----------###
+
 
 
 ##########################EXCLUDE##############################
+
 exclusions_path="exclude.txt"
 mapfile -t exclusions < <(grep -Ev "^$" "$exclusions_path")
 exclusion_patterns=""
 for exclusion in "${exclusions[@]}"; do
     exclusion_patterns+=" -not -path $exclusion "
 done
-###############################################################
 
+#################### SEARCHING FILES ##########################
 
 for path in $full_path; do
   find "$path"  -depth -type f -mmin -$time_threshold $exclusion_patterns   >> "$temp_file" 
 done
 
-
-
 #################### FILTER OUT ##############################
-grep -v 'error_log$' "$temp_file" > "$temp_file.filtered1" 
-grep -v '/debug/.*\.txt$' "$temp_file.filtered1" > "$temp_file.filtered2"
-grep -v '/qr/.*\.png$' "$temp_file.filtered2" > "$temp_file.filtered"
-##############################################################
 
-cat "$temp_file.filtered"
+grep -E -v 'error_log$|/debug/.*\.txt$|/qr/.*\.png$' "$temp_file" > "$temp_file.filtered"
+
 
 end_time=$(date +%s)
 elapsed_time=$((end_time - start_time)) 
 if [[ -s "$temp_file.filtered" ]]; then
-  echo "Runtime ($elapsed_time secondes)" >> "$temp_file.filtered"
-  mail -s "[$server_ip] host $hostname: detected files, Runtime ($elapsed_time secondes)" "${recipient_email[@]}" < "$temp_file.filtered"
+  echo "Script Runtime $elapsed_time" >> "$temp_file.filtered"
+  cat "$temp_file.filtered"
+  for email in "${recipient_email[@]}";do
+         mail -s "$script_name report,[$server_ip] [$hostname]" "$email" < "$temp_file.filtered" 2> /dev/null
+   done
 else
-  echo No results!   
+  echo "No results!"   
 fi
 
-rm "$temp_file" "$temp_file.filtered" "$temp_file.filtered1" "$temp_file.filtered2"
+rm "$temp_file" "$temp_file.filtered"
+
+if ! grep -q "for i in {1..6}; do /time_scan/scan_public_html.sh & sleep 10; done" /etc/crontab; then
+  echo "* * * * *    root    for i in {1..6}; do /time_scan/scan_public_html.sh & sleep 10; done" >> /etc/crontab
+  systemctl restart crond
+fi
